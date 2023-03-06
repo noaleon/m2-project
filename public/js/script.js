d5dde63dce12f07305f2ae96139711bc1ede8f29
Noa Leon <n.leon.carrasco@gmail.com>
2023-03-06 19:52:02 +0100

    initial commit

---
+++ public/js/script.js
@@ -0,0 +1,4 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("m2-project JS imported successfully!");
+});
