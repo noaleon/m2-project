b8088583e4d9f03d7f64f43233f0efe36d139c35
Fernanda Souza <fecafumee@gmail.com>
2023-03-07 20:11:41 +0100

    done user and project models

---
+++ models/Project.model.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+
+// TODO: Please make sure you edit the User model to whatever makes sense in this case
+const projectSchema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    skills: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Project = model('Project', projectSchema);
+
+module.exports = Project;
--- models/User.model.js
+++ models/User.model.js
@@ -1,32 +1,45 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose');
 
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new Schema(
   {
-    username: {
+    fullName: {
       type: String,
       trim: true,
-      required: false,
-      unique: true
+      required: true,
+      unique: true,
     },
     email: {
       type: String,
       required: true,
+      match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.'],
       unique: true,
       lowercase: true,
-      trim: true
+      trim: true,
     },
     password: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
+    phoneNumber: {
+      type: Number,
+      unique: true,
+    },
+    skills: {
+      type: String,
+      default: [],
+    },
+    role: {
+      type: ['user', 'artist'],
+    },
+    location: String,
+    profession: String,
   },
   {
-    // this second object adds extra properties: `createdAt` and `updatedAt`    
-    timestamps: true
-  }
+    timestamps: true,
+  },
 );
 
-const User = model("User", userSchema);
+const User = model('User', userSchema);
 
 module.exports = User;
